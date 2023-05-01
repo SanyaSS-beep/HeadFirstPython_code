f45769e75d7077dc14d78a26a4b563a3959273be
SanyaSS-beep <maoshuaishuai0424@126.com>
2023-05-01 21:28:31 +0800

    Replace all found short horizontal lines or colons with a dot.

--- read_data.py
+++ read_data.py
@@ -12,10 +12,34 @@ def read_data():
         data = saf.readline()
     sarah = data.strip().split(',')
 
-    print(sorted(james))
-    print(sorted(julie))
-    print(sorted(mikey))
-    print(sorted(sarah))
+    clean_james = []
+    clean_julie = []
+    clean_mikey = []
+    clean_sarah = []
+
+    for each_t in james:
+        clean_james.append(sanitize(each_t))
+    for each_t in julie:
+        clean_julie.append(sanitize(each_t))
+    for each_t in mikey:
+        clean_mikey.append(sanitize(each_t))
+    for each_t in sarah:
+        clean_sarah.append(sanitize(each_t))
+
+    print(sorted(clean_james))
+    print(sorted(clean_julie))
+    print(sorted(clean_julie))
+    print(sorted(clean_sarah))
+
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = ''
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return(time_string)
+    (mins, secs) = time_string.split(splitter)
+    return(mins + '-.' + secs)
 
 if __name__ == '__main__':
     read_data()
