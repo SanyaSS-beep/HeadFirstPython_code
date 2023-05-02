bc0f06f0d9d1af87c1746155491a51b7350a2a98
SanyaSS-beep <maoshuaishuai0424@126.com>
2023-05-02 09:29:23 +0800

    replace dict, define Athlete class

--- read_data.py
+++ read_data.py
@@ -4,13 +4,13 @@ def read_data():
     mikey = get_coach_data('./data/mikey2.txt')
     sarah = get_coach_data('./data/sarah2.txt')
 
-    print(james['name'] + "'s fastest times are: "+str(james['times']))
+    print(james.name + "'s fastest times are: "+str(james.top3()))
 
-    print(julie['name'] + "'s fastest times are: "+str(julie['times']))
+    print(julie.name + "'s fastest times are: "+str(julie.top3()))
     
-    print(mikey['name'] + "'s fastest times are: "+str(mikey['times']))
+    print(mikey.name + "'s fastest times are: "+str(mikey.top3()))
 
-    print(sarah['name'] + "'s fastest times are: "+str(sarah['times']))
+    print(sarah.name + "'s fastest times are: "+str(sarah.top3()))
 
 def sanitize(time_string):
     if '-' in time_string:
@@ -27,15 +27,18 @@ def get_coach_data(filename):
         with open(filename) as f:
             data = f.readline()
         templ = data.strip().split(',')
-        return ({
-            'name': templ.pop(0),
-            'DOB': templ.pop(0),
-            'times': str(sorted(set([sanitize(t) for t in templ]))[0:3])
-        })
+        return Athlete(templ.pop(0), templ.pop(0), templ)
     except IOError as ioerr:
         print('File error:'+str(ioerr))
         return(None)
     
-
+class Athlete:
+    def __init__(self, a_name, a_dob=None, a_times=[]) -> None:
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times
+    
+    def top3(self):
+        return (sorted(set([sanitize(t) for t in self.times]))[0:3])
 if __name__ == '__main__':
     read_data()
