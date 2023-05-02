02c8c749ab1c50afdfc448c4ca6b615b73bb0284
SanyaSS-beep <maoshuaishuai0424@126.com>
2023-05-02 09:31:41 +0800

    add an additional timing value to the runner's timing data

--- read_data.py
+++ read_data.py
@@ -40,5 +40,12 @@ class Athlete:
     
     def top3(self):
         return (sorted(set([sanitize(t) for t in self.times]))[0:3])
+
+    def add_time(self, time_value):
+        self.times.append(time_value)
+
+    def add_times(self, list_of_times):
+        self.times.extend(list_of_times)
+        
 if __name__ == '__main__':
     read_data()
