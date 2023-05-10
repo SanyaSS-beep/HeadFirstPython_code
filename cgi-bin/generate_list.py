c91698a512bb7b1d69c558af8be4eaaebe1d3b0f
SanyaSS-beep <maoshuaishuai0424@126.com>
2023-05-10 16:46:32 +0800

    temporatory code

--- cgi-bin/generate_list.py
+++ cgi-bin/generate_list.py
@@ -10,11 +10,11 @@ print(athletes)
 
 print(yate.start_response())
 print(yate.include_header("Coach Kelly's List of Athletes"))
-print(yate.start_form("generate_timeing_data.py"))
+print(yate.start_form("generate_timing_data.py"))
 print(yate.para("Select an athlete from the list to work with:"))
 
 for each_athlete in athletes:
-    print(yate.radio_button("Which athlete:", athletes[each_athlete].name))
+    print(yate.radio_button("which_athlete:", athletes[each_athlete].name))
 print(yate.end_form("Select"))
 
 print(yate.include_footer({"Home": "/index.html"}))
